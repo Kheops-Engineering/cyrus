2bfc7a7d4692295dad120942fa70a5f8f0e3b7e7
Med Mouine <med@mohameds-MacBook-Pro.local>
2020-06-28 02:08:32 -0400

    Setup mono repo and add base for web workspace

---
+++ web/webpack.development.js
@@ -0,0 +1,12 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'source-map'
+});
